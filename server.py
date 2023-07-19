26230023e24da9620cd16cd709efb47935194f43
mahesh796 <maheshnec7@gmail.com>
2023-07-19 15:29:21 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,66 @@
+import os
+import replicate
+import streamlit as st
+
+from streamlit.logger import get_logger
+
+logger = get_logger(__name__)
+
+st.title("Llama-v2 Chat Demo with Message History")
+st.markdown("Built by Mahesh M")
+os.environ["REPLICATE_API_TOKEN"] = "r8_3clbgC4BaY6LalFxGULXFCkO10D3pOo0u5orQ"
+llama_family = {
+    "Llama7B-v2-Chat": "a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea",
+    "Llama13B-v2-Chat": "a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
+    "Llama70B-v2-Chat": "replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48",
+}
+model_choice = st.selectbox("Model", options=tuple(llama_family.keys()))
+llm_model = llama_family[model_choice]
+st.session_state["llm_model"] = llm_model
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+with st.sidebar:
+    st.markdown("## Model Parameters")
+    # repetition_penalty = st.slider("Repetition Penalty", 0.0, 2.0, 1.0, help="Penalty for repeated words in generated text; 1 is no penalty, values greater than 1 discourage repetition, less than 1 encourage it.")
+    temperature = st.slider("Temperature", 0.01, 5.0, 0.9, help="Adjusts randomness of outputs, greater than 1 is random and 0 is deterministic, 0.75 is a good starting value.")
+    max_tokens = st.slider("Max Length in Number of Tokens", 10, 500, 100, help="Maximum number of tokens to generate. A word is generally 2-3 tokens")
+    top_p = st.slider("Top P", 0.01, 1.0, 0.2, help="When decoding text, samples from the top p percentage of most likely tokens; lower to ignore less likely tokens")
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+def get_message_history():
+    for message in st.session_state.messages:
+        role, content = message["role"], message["content"]
+        yield f"{role.title()}: {content}"
+
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        message_history = "\n".join(list(get_message_history())[-3:])
+        logger.info(f"Message History: {message_history}")
+        output = replicate.run(
+            llm_model,
+            input={
+                "prompt": f"{message_history}\nAssistant:",
+                "max_tokens": max_tokens,
+                "temperature": temperature,
+                "top_p": top_p,
+                # "repetition_penalty": repetition_penalty,
+                "debug": True,
+            },
+        )
+    for item in output:
+        full_response += item
+        message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
